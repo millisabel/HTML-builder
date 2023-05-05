523d3a0c84d7e874b42e0fa677730402187053ab
millisabel <elena.kozlova.job@gmail.com>
2023-05-05 23:49:35 +0200

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const sourceDir = path.join(__dirname, 'files');
+  const targetDir = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.mkdir(targetDir, { recursive: true });
+
+    const files = await fs.readdir(sourceDir);
+
+    for (const file of files) {
+      const sourceFile = path.join(sourceDir, file);
+      const targetFile = path.join(targetDir, file);
+
+      const stats = await fs.stat(sourceFile);
+
+      if (stats.isFile()) {
+        await fs.copyFile(sourceFile, targetFile);
+      }
+    }
+
+    console.log('Copying completed!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir();
