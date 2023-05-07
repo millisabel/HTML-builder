4137808809634288e16166f532789595882566b8
millisabel <elena.kozlova.job@gmail.com>
2023-05-08 00:05:11 +0200

    fix: corrected task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,23 +4,32 @@ const path = require('path');
 const stylesDir = path.join(__dirname, 'styles');
 const distDir = path.join(__dirname, 'project-dist');
 
-fs.mkdirSync(distDir, { recursive: true });
-
-fs.readdir(stylesDir, (err, files) => {
+fs.mkdir(distDir, { recursive: true }, (err) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  const cssFiles = files.filter(file => path.extname(file) === '.css');
+  fs.readdir(stylesDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-  const styles = cssFiles.map(file => {
-    const filePath = path.join(stylesDir, file);
-    return fs.promises.readFile(filePath, 'utf8');
+    const cssFiles = files.filter(file => path.extname(file) === '.css');
+
+    const styles = cssFiles.map(file => {
+      const filePath = path.join(stylesDir, file);
+      return fs.promises.readFile(filePath, 'utf8');
+    });
+
+    Promise.all(styles)
+      .then(results => {
+        const distFilePath = path.join(distDir, 'bundle.css');
+        const cssString = results.join('\n');
+        return fs.promises.writeFile(distFilePath, cssString);
+      })
+      .then(() => console.log('Styles merged successfully'))
+      .catch(err => console.error(err));
   });
-
-  const distFilePath = path.join(distDir, 'bundle.css');
-  fs.promises.writeFile(distFilePath, styles.join('\n'))
-    .then(() => console.log('Styles merged successfully'))
-    .catch(err => console.error(err));
 });
